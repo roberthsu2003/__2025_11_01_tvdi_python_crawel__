a79f38c559205b27d367955e94022fb7237c3854
徐國堂 <roberthsu2003@gmail.com>
2025-11-29 09:48:16 +0800

    modify

--- lesson5/lesson5_1.py
+++ lesson5/lesson5_1.py
@@ -1,4 +1,6 @@
 import os
+from playwright.sync_api import sync_playwright
+
 def get_html_path()-> str:
     """返回HTML文件的絕對路徑"""
     current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -7,7 +9,18 @@ def get_html_path()-> str:
 
 def main():
     path = get_html_path()
-    print(path)
+    with sync_playwright() as p:
+        # 啟動瀏覽器
+        browser = p.chromium.launch(headless=False, slow_mo=500)
+
+        # 打開新頁面
+        page = browser.new_page()
+
+        page.goto(path)
+
+        page.wait_for_timeout(3000)  # 等待3秒以觀察效果
+
+        browser.close()
 
 if __name__ == "__main__":
     main()
