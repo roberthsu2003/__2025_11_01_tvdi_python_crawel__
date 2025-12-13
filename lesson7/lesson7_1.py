dfab3f72992cbce956c0b27e1dc5c36f44d0a1e5
徐國堂 <roberthsu2003@gmail.com>
2025-12-13 10:07:29 +0800

    modify

---
+++ lesson7/lesson7_1.py
@@ -0,0 +1,17 @@
+import asyncio
+from crawl4ai import AsyncWebCrawler,CrawlerRunConfig,CacheMode
+
+async def main():
+    html = """
+<div class="item>
+    <h2>項目1</h2>
+    <a href="https://example.com/item1">連結1</a>
+</div>"""
+    run_config = CrawlerRunConfig(cache_mode=CacheMode.BYPASS)
+    async with AsyncWebCrawler() as crawler:
+        result = await crawler.arun(
+            url="https://example.com",
+            config=run_config)
+
+if __name__ == "__main__":
+    asyncio.run(main())
